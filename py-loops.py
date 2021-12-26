b63ca814f60b57f2a464f5488f7324d4d0cf7066
Sona <ebrazi.sona@gmail.com>
2021-12-26 23:18:14 +0330

    python loops

---
+++ py-loops.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i*i)
