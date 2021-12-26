067e1bad1cdbde1d9f13ed784692be6e644b376b
Sona <ebrazi.sona@gmail.com>
2021-12-26 23:05:30 +0330

    arithmetic-op

---
+++ arithmetic-oper.py
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    add=a+b
+    dif=a-b
+    product=a*b
+    print(add)
+    print(dif)
+    print(product)
