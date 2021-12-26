c376519e6d40ff9da261b59f93d4a220317388c4
Sona <ebrazi.sona@gmail.com>
2021-12-26 23:11:11 +0330

    python-division

---
+++ python-division.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
