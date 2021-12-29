5bba0dd68f2737d166c9721eab8fc6f89f47c8d3
Sona <ebrazi.sona@gmail.com>
2021-12-29 15:17:55 +0330

    integers size

---
+++ integers.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print((a**b)+(c**d))
