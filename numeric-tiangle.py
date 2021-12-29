a41c27c81a2c3d04733710449f39366fc9311824
Sona <ebrazi.sona@gmail.com>
2021-12-29 15:32:02 +0330

    numerical-triangle

---
+++ numeric-tiangle.py
@@ -0,0 +1,4 @@
+#https://www.hackerrank.com/challenges/python-quest-1/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#More than 2 lines will result in 0 score. Do not leave a blank line also
+for i in range(1,int(input())):
+    print(int((10**i-1)/9)*i)
