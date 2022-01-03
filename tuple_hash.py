38a9932ee943cdbe96db9320d991035179373d43
Sona <ebrazi.sona@gmail.com>
2022-01-03 23:07:32 +0330

    tuple-hash

---
+++ tuple_hash.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/python-tuples/problem?isFullScreen=true
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    t=tuple(integer_list)
+    print(hash(t))
