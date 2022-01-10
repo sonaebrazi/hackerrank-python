0f121e9bde2b154b9c0eab02d28e2b4aca79d5d7
Sona <ebrazi.sona@gmail.com>
2022-01-10 19:38:06 +0330

    validating roman number

---
+++ validate-roman-number.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/validate-a-roman-number/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"	# Do not delete 'r'.
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
